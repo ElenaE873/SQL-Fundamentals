20489d722695f91050032bf7ada76061f2fa0747
ElenaE873 <68255140+ElenaE873@users.noreply.github.com>
2022-01-26 18:46:35 -0500

    New file.

---
+++ JOINS/6_Outer-Right-Join.sql
@@ -0,0 +1,12 @@
+/* (Outer) Right - Join
+
+This is essentiall a flip of the Left - Join.
+All the records in the right-table are key, and data from the left join is brough over
+where there is a match by unique key to the right table.  Any non-matches will result in 
+missing values in the left-table columns. */
+
+/* something to remember for both LEFT and RIGHT-Joins
+
+the "right-table" will always come after the "JOIN" clause
+the "left-table" will always come after the FROM statement */
+
